be5d138e9b62a5b1e641818fe7d12db17a4ddcd2
EmptyRamenBox <alexis.martinezo92@gmail.com>
2020-04-09 11:48:35 -0500

    Added "NavBar"

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title class="font-weight-light">
+        <span>My</span>
+        <span class="font-weight-regular">Blog</span>
+      </v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
